829a79ce81da7ab1ab672cbe3e46ab343564f985
las3qj <las3qj@virginia.edu>
2021-01-27 16:50:01 -0500

    bugfix (4)

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
         {!session && (
           <>
             <h4> Sign in with Google for quick, easy access! <br/> Or check out the live demo to see some example notes. </h4>
-            <Button variant="primary-2" size="lg" onClick={signIn('google')}> Sign in </Button>
+            <Button variant="primary-2" size="lg" onClick={()=>signIn('google')}> Sign in </Button>
             <Button variant="success-2" size="lg" href="/mainpage"> Explore demo </Button>
           </>
         )}
@@ -31,7 +31,7 @@ export default function Home() {
           <>
             <h4> You're currently signed in as {""+session.user.email}!<br/> Continue to begin writing and organizing or sign out to view the demo.</h4>
             <Button variant="primary-2" href="/mainpage" size="lg"> Continue </Button>
-            <Button variant="success-2" size="lg" onClick={signOut}> Sign out </Button>
+            <Button variant="success-2" size="lg" onClick={()=>signOut}> Sign out </Button>
           </>
         )}
       </Jumbotron>
